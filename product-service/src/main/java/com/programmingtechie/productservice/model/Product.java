1eed59e1dc32e49e7ba818ffc5cde317de3f429b
User <keith.omiping@ifis.com.sg>
2023-09-10 02:20:36 +0800

    Initial commit

---
+++ product-service/src/main/java/com/programmingtechie/productservice/model/Product.java
@@ -0,0 +1,35 @@
+package com.programmingtechie.productservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "product")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class Product implements Serializable {
+
+    private static final long serialVersionUID = 5783944569833008143L;
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price")
+    private BigDecimal price;
+}
